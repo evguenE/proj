67384d0646245315c236c74ca5fede2b2d3fd395
evg <evguen@rambler.ru>
2018-08-21 08:15:43 +0300

    Добавление record 7

--- dbo.GuestList.sql
+++ dbo.GuestList.sql
@@ -9,13 +9,12 @@
 
 
 SET IDENTITY_INSERT [dbo].[Guests] ON
-
-INSERT INTO [dbo].[Guests] ([Id], [fio], [email], [phone], [flag]) VALUES (1, N'Project', N'evguen@rambler.ru', N'89056785', N'0')
 INSERT INTO [dbo].[Guests] ([Id], [fio], [email], [phone], [flag]) VALUES (1, N'Иванов Иван Иванович', N'evguen@rambler.ru', N'89056785', N'0')
 INSERT INTO [dbo].[Guests] ([Id], [fio], [email], [phone], [flag]) VALUES (2, N'Сидоров Сергей Сергеевич', N'sidor@yandex.ru', N'89066678', N'1')
 INSERT INTO [dbo].[Guests] ([Id], [fio], [email], [phone], [flag]) VALUES (3, N'Петров петр ', N'peter@mail.com', N'3333333', N'2')
 INSERT INTO [dbo].[Guests] ([Id], [fio], [email], [phone], [flag]) VALUES (4, N'Спиридонов', N'ty@yuyu.net', N'8911907867', N'0')
 INSERT INTO [dbo].[Guests] ([Id], [fio], [email], [phone], [flag]) VALUES (5, N'Назарет', N'ee@ii.org', N'89054567', N'2')
 INSERT INTO [dbo].[Guests] ([Id], [fio], [email], [phone], [flag]) VALUES (6, N'Стивенсон', N'sss@uuu.org', N'66666', N'1')
+INSERT INTO [dbo].[Guests] ([Id], [fio], [email], [phone], [flag]) VALUES (7, N'Proj', N'evguen@rambler.ru', N'89056785', N'0')
 SET IDENTITY_INSERT [dbo].[Guests] OFF
 
